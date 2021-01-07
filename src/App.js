8a1f0a4c53104ee990b06d5a62ee37fe851518ff
Andrew Murray <andyranted@gmail.com>
2021-01-06 20:11:08 -0500

    Cleanup of syntax to generate menu.

--- src/App.js
+++ src/App.js
@@ -5,24 +5,22 @@ import ListItem from '@material-ui/core/ListItem';
 import Button from '@material-ui/core/Button';
 
 function App() {
+  const menuItems = [
+    "Create Room",
+    "Join Room",
+    "Practice",
+    "Settings"
+  ];
+  const listItems = menuItems.map( (text) =>
+    <ListItem key={text}>
+        <Button variant="contained" style={{width: "100%"}} aria-label={text}>{text}</Button>
+    </ListItem>
+  );
+
   return (
     <div className="App">
         <List aria-label="menu">
-          <ListItem key="Create Room">
-              <Button variant="contained" style={{width: "100%"}} aria-label="Create Room">Create Room</Button>
-          </ListItem>
-
-          <ListItem key="Join Room">
-              <Button variant="contained" style={{width: "100%"}} aria-label="Join Room">Join Room</Button>
-          </ListItem>
-
-          <ListItem>
-              <Button variant="contained" style={{width: "100%"}} aria-label="Practice">Practice</Button>
-          </ListItem>
-
-          <ListItem key="Settings">
-              <Button variant="contained" style={{width: "100%"}} aria-label="Settings">Settings</Button>
-          </ListItem>
+          {listItems}
         </List>
     </div>
   );
