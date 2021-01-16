ee117d446e941795c5d30d389605ee122341dadf
Andrew Murray <andyated@gmail.com>
2021-01-15 19:11:00 -0500

    Implement some things for deploying to heroku.

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,7 @@
 import io from "socket.io-client";
+
 const path = new URL( window.location.origin );
-const ENDPOINT = path.origin + ":8080";
+const ENDPOINT = process.env.SERVER_ENDPOINT ? process.env.SERVER_ENDPOINT
+                                             : "ws://" + path.hostname + ":8080";
 let socket = io(ENDPOINT);
 export default socket;
