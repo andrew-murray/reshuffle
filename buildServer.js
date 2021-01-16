40e20c28c5da019f104561ec271a169170feeaf4
Andrew Murray <andyated@gmail.com>
2021-01-16 14:54:03 -0500

    Amend build server configuration. Configurable port, ensure anything reshuffle/* maps to the react-app.

--- buildServer.js
+++ buildServer.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use("/reshuffle", express.static(path.join(__dirname, 'build')));
 
-app.get('/reshuffle', function (req, res) {
+app.get('/reshuffle/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`server running at http://localhost:${port}/`);
+});
