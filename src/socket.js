e332520a2caa958c36f84b1d8243a59c5faa4c38
Andrew Murray <andyated@gmail.com>
2021-01-16 14:03:08 -0500

    Fix error in debug server, don't promote to https when we aren't serving at https.

--- src/socket.js
+++ src/socket.js
@@ -3,6 +3,6 @@ import io from "socket.io-client";
 const path = new URL( window.location.origin );
 const ENDPOINT = process.env.NODE_ENV === 'production' ? process.env.SERVER_ENDPOINT ? process.env.SERVER_ENDPOINT
                                                                                      : "wss://reshuffle.herokuapp.com/"
-                                                       : "wss://" + path.hostname + ":8080";
+                                                       : path.protocol + "//" + path.hostname + ":8080";
 let socket = io(ENDPOINT);
 export default socket;
