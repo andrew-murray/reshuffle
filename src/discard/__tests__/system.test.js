3ed1377f8055809b210c6533be55951934ce72d5
Andrew Murray <andyated@gmail.com>
2021-01-17 17:04:33 -0500

    Testing these connections is a nightmare.

--- src/discard/__tests__/system.test.js
+++ src/discard/__tests__/system.test.js
@@ -9,10 +9,35 @@ afterAll((done)=>{
 })
 
 describe('basic othello example', () => {
-  test('blargh',  (done) => {
+  test('othello.join',  () => {
+    const roomID = "my-silly-room";
     let sessionData = {};
     system.othello.initialiseStorage(sessionData);
-    return config.createSockets(io, system.server, 2)
-    .then((sockets)=>{done();});
+    return config.createSockets(io, system.server, 2).then((sockets)=>{
+      // assert current state
+      expect(sessionData).toMatchObject({});
+      expect(sockets[0].eventHistory).toStrictEqual([]);
+      expect(sockets[1].eventHistory).toStrictEqual([]);
+
+      sockets[0].emit("othello.join", roomID);
+      sockets[1].emit("othello.join", roomID);
+
+      const successCriteria = [...Array(sockets.length).keys()].map(
+        // expect 2 state updates for player 0 and 1 for player 2
+        i => ()=>{
+            return sockets[i].eventHistory.length >= sockets.length - i - 1;
+        }
+      );
+
+      return config.awaitConditionsOnSockets(
+        sockets,
+        successCriteria
+      );
+    })
+    .then((sockets)=>{
+      expect(sockets[0].eventHistory.length).toBe(2);
+      expect(sockets[1].eventHistory.length).toBe(1);
+      return config.disconnectAll(system.io, sockets);
+    });
   });
 });
--- src/discard/testConfig.js
+++ src/discard/testConfig.js
@@ -53,13 +53,20 @@ const createSocket = (client, httpServer) => {
   return new Promise((resolve, reject) => {
     // Setup
     let httpServerAddr = httpServer.address();
-    let sockets = [];
     // Do not hardcode server port and address, square brackets are used for IPv6
-    socket = client.connect(`http://[${httpServerAddr.address}]:${httpServerAddr.port}`, {
+    let socket = client.connect(`http://[${httpServerAddr.address}]:${httpServerAddr.port}`, {
       'reconnection delay': 0,
       'reopen delay': 0,
       'force new connection': true,
       transports: ['websocket'],
+    });
+    socket.eventHistory = [];
+    // listen to any events
+    socket.onAny((event, ...args)=>{
+      socket.eventHistory.push({
+          event: event,
+          arguments: args
+      });
     });
     socket.once('connect', ()=>{resolve(socket);});
   });
@@ -69,7 +76,58 @@ const createSockets  = (client, httpServer, n) => {
   return Promise.all(
     [...Array(n).keys()].map(i=>createSocket(client,httpServer))
   );
-}
+};
+
+const disconnectAll = (io, sockets) =>
+{
+  return new Promise((resolve, reject) => {
+    let remainingSocketIDs = sockets.map(s=>s.id);
+    const disconnectCallback = (id)=>{
+      const indexID = remainingSocketIDs.indexOf(id);
+      if(indexID >= 0)
+      {
+        remainingSocketIDs.splice(indexID, 1);
+      }
+      if(remainingSocketIDs.length === 0){
+        resolve(sockets);
+      }
+    };
+    for( const socketID of remainingSocketIDs )
+    {
+      io.sockets.sockets.get(socketID).on(
+        "disconnect",
+        ()=>{disconnectCallback(socketID);}
+      );
+    }
+    for(s of sockets)
+    {
+      s.disconnect(true);
+    }
+  });
+};
+
+const awaitConditionOnSocket = (socket,callback) => {
+  return new Promise((resolve, reject) => {
+    socket.onAny(()=>{
+      if(callback(socket))
+      {
+        resolve(socket);
+      }
+    });
+  });
+};
+
+const awaitConditionOnSockets = (sockets, callback) => {
+  return Promise.all(
+    sockets.map(socket=>awaitConditionOnSocket(socket, callback))
+  );
+};
+
+const awaitConditionsOnSockets = (sockets, callbacks) => {
+  return Promise.all(
+    [...Array(sockets.length).keys()].map(i=>awaitConditionOnSocket(sockets[i], callbacks[i]))
+  );
+};
 
 /**
  * Run after each test
@@ -86,7 +144,12 @@ globals.beforeEach = beforeEach;
 globals.beforeAll = beforeAll;
 globals.afterEach = afterEach;
 globals.afterAll = afterAll;
+
 globals.createSocket = createSocket;
 globals.createSockets = createSockets;
+globals.disconnectAll = disconnectAll;
+globals.awaitConditionOnSocket = awaitConditionOnSocket;
+globals.awaitConditionOnSockets = awaitConditionOnSockets;
+globals.awaitConditionsOnSockets = awaitConditionsOnSockets;
 
 module.exports = globals;
