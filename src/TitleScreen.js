294a2e7578fd616a021cb2b207548aa0d40902da
Andrew Murray <andyated@gmail.com>
2021-01-20 17:19:51 -0500

    Add a spinner while waiting for the backend to start up.

--- src/TitleScreen.js
+++ src/TitleScreen.js
@@ -6,7 +6,13 @@ import TitledDialog from "./TitledDialog";
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Paper from '@material-ui/core/Paper';
 import socket from "./socket";
 
 import SessionTable from "./SessionTable";
@@ -16,7 +22,7 @@ const styler = (theme) => ({
     '& > *': {
       margin: theme.spacing(1),
     },
-  },
+  }
 });
 
 const infoStyles = {
@@ -31,18 +37,23 @@ class TitleScreen extends React.Component
   state = {
     rooms: [],
     joinDialogOpen: false,
-    infoDialogOpen: false
+    infoDialogOpen: false,
+    connectedToServer: false
   }
 
   constructor(props)
   {
     super(props);
     this.updateRoomData = (roomData)=>{this.setState({rooms: roomData.rooms});};
+    this.updateConnectedStatus = ()=>{this.setState({connectedToServer: socket.connected});};
   }
 
 
   componentDidMount()
   {
+    this.updateConnectedStatus();
+    socket.on("connect", this.updateConnectedStatus);
+    socket.on("disconnect", this.updateConnectedStatus);
     socket.on("othello.rooms.list", this.updateRoomData);
     socket.emit("othello.rooms.request");
     this.interval = setInterval(
@@ -53,6 +64,8 @@ class TitleScreen extends React.Component
 
   componentWillUnmount()
   {
+    socket.off("connect", this.updateConnectedStatus);
+    socket.off("disconnect", this.updateConnectedStatus);
     socket.off("othello.rooms.list", this.updateRoomData);
     clearInterval(
       this.interval
@@ -82,16 +95,33 @@ class TitleScreen extends React.Component
     };
 
     return (
-      <main>
+      <div>
         <SessionTable
           onJoin={joinRoom}
           rooms={this.state.rooms}
           />
-        <div className={classes.actionGroup}>
-          {makeButton("Create Room", this.props.onCreate)}
-          {makeButton("Find Room", ()=>{this.setState( {joinDialogOpen: true} );})}
-          {makeButton("Practice", ()=>{ history.push("/othello/practice"); })}
-        </div>
+        {this.state.connectedToServer &&
+          <div className={classes.actionGroup}>
+            {makeButton("Create Room", this.props.onCreate)}
+            {makeButton("Find Room", ()=>{this.setState( {joinDialogOpen: true} );})}
+            {makeButton("Practice", ()=>{ history.push("/othello/practice"); })}
+          </div>
+        }
+        {!this.state.connectedToServer &&
+          <div style={{width:"100%", display: "flex", justifyContent: "center", paddingTop: "1vh"}}>
+            <Card style={{maxWidth: 350}}>
+              <Paper>
+                <CardMedia>
+                  <CircularProgress style={{marginTop:"3vh"}}/>
+                </CardMedia>
+                <CardContent>
+                  <Typography>This demo uses a low-cost backend server which starts up on demand.</Typography>
+                  <Typography>If no one else is playing, there'll typically be a 30 second delay before the server is ready.</Typography>
+                </CardContent>
+              </Paper>
+            </Card>
+          </div>
+        }
         <TextEntryDialog
           open={this.state.joinDialogOpen}
           text="Enter room name"
@@ -117,7 +147,7 @@ class TitleScreen extends React.Component
             <InfoIcon color="primary"/>
           </IconButton>
         </div>
-      </main>
+      </div>
     );
   }
 }
