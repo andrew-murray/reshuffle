99846b9554fff1b3b8835be52311341707da5674
Andrew Murray <andyated@gmail.com>
2021-01-22 17:19:02 -0500

    Modify chat.join to take a name and chat.create not to auto-join the room.

--- src/discard/chatSession.js
+++ src/discard/chatSession.js
@@ -29,8 +29,7 @@ function createChatIfNecessary(roomID)
   if(!(roomID in sessionData))
   {
     debug("creating chat " + roomID);
-    sessionData[roomID] = {history: new CircularBuffer(100)};
-    sessionData[roomID].history.push("Room " + roomID + " was created.")
+    sessionData[roomID] = {history: new CircularBuffer(100), names: {}};
   }
 }
 
@@ -44,15 +43,16 @@ function deleteChatIfNecessary(io, roomID)
   }
 }
 
-function joinChatRoom(io, socket, roomID)
+function joinChatRoom(io, socket, roomID, name)
 {
   createChatIfNecessary(roomID);
+
+  sessionData[roomID].names[socket.id] = name;
 
   const existingMessages = getChatHistory(roomID);
 
   socket.on('disconnect', reason => {
     deleteChatIfNecessary(io, roomID);
-    socket.to(roomID).emit('chat message', "someone left...");
   });
 
   socket.on('chat.message', msg => {emitChatMessage(io, roomID, msg);});
@@ -61,13 +61,12 @@ function joinChatRoom(io, socket, roomID)
   {
     socket.emit('chat.message', msg);
   }
-  socket.to(roomID).emit('chat.message', "someone joined!")
 }
 
 function subscribe(io,socket)
 {
   socket.on('chat.create', (optionalID) => {
-    debug("socket " + socket.id + " requeste to create " + optionalID ? optionalID : "");
+    debug("socket " + socket.id + " requested to create " + optionalID ? optionalID : "");
     if(optionalID)
     {
       if(socket.rooms.has(optionalID))
@@ -81,11 +80,6 @@ function subscribe(io,socket)
         // todo: this may/may not be the correct socket.io idiom
         socket.emit('error', msg);
       }
-      else
-      {
-        socket.join(optionalID);
-        joinChatRoom(io, socket, optionalID)
-      }
       return;
     }
     const vacantRoom = rooms.findVacantRoom(io);
@@ -97,18 +91,16 @@ function subscribe(io,socket)
       socket.emit('error', msg);
       return;
     }
-    socket.join(vacantRoom);
-    joinChatRoom(io, socket, vacantRoom);
     debug("created " + vacantRoom);
     socket.emit("room.created", vacantRoom);
   });
 
-  socket.on('chat.join', (roomID)=>{
+  socket.on('chat.join', (roomID, name)=>{
     debug("socket " + socket.id + " requested to join " + roomID );
     if(!socket.rooms.has(roomID))
     {
       socket.join(roomID);
-      joinChatRoom(io,socket,roomID);
+      joinChatRoom(io,socket,roomID,name);
     }
   });
 }
