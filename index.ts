8607ae38d5f99e5d90d2d7e05604eccbde47b91a
Andrew Murray <andyated@gmail.com>
2021-04-04 13:41:06 -0400

    Add module entry points.

---
+++ index.ts
@@ -0,0 +1,4 @@
+export {TitleScreen} from "./src/TitleScreen";
+export {OfflineOthelloGame} from "./src/games/OfflineOthelloGame"
+export {OnlineOthelloGame} from "./src/games/OnlineOthelloGame"
+export {socket} from "./src/socket"
